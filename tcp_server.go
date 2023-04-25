bb9fc89d8e71fc5df238f5b971d3742d288156ff
TonyLe02 <tonynl@student.uia.no>
2023-04-25 15:58:36 +0200

    Update tcp_server.go

--- tcp_server.go
+++ tcp_server.go
@@ -41,7 +41,8 @@ func main() {
 					}
 					dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
 					log.Println("Dekrypter melding: ", string(dekryptertMelding))
-					switch msg := string(dekryptertMelding); msg {  				        
+					switch msg := string(dekryptertMelding); msg {
+						  				        
 					case "ping":
 						_, err = c.Write([]byte("pong"))
 
